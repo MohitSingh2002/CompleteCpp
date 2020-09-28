0654eb6c5be22736a193c59559a368c1fc274f05
Mohit <mohitsingh11082002@gmail.com>
2020-09-28 20:13:53 +0530

    Added circular linked list support.

--- Data Structires And Algorithms/3_circular_linked_list/1_circular_linked_list.cpp
+++ Data Structires And Algorithms/3_circular_linked_list/1_circular_linked_list.cpp
@@ -1,7 +1,22 @@
 #include<bits/stdc++.h>
+#include"1_circular_linked_list_helper.h"
 using namespace std;
 
 int main() {
+
+    CircularLinkedList list;
+
+    for(int i=1;i<=5;i++) {
+        list.addLast(i);
+    }
+    for(int i=6;i<=10;i++) {
+        list.addFirst(i);
+    }
+
+    cout << "The list after above operations are : " << endl;
+    list.displayList();
+
+    cout << "The length of the list is : " << list.displayLength() << endl;
     
     return 0;
 }
--- Data Structires And Algorithms/3_circular_linked_list/1_circular_linked_list_helper.h
+++ Data Structires And Algorithms/3_circular_linked_list/1_circular_linked_list_helper.h
@@ -19,6 +19,48 @@ class Node {
 
 class CircularLinkedList {
 
-    Node *head = NULL;
+    public:
+        Node *last = NULL;
+        int length = 0;
+
+        void addLast(int value) {
+            Node *toAdd = new Node();
+            toAdd->data = value;
+            if(last == NULL) {
+                last = toAdd;
+                last->next = last;
+            } else {
+                toAdd->next = last->next;
+                last->next = toAdd;
+                last = toAdd;
+            }
+            length++;
+        }
+
+        void addFirst(int value) {
+            Node *toAdd = new Node();
+            toAdd->data = value;
+            if(last == NULL) {
+                last = toAdd;
+            } else {
+                toAdd->next = last->next;
+            }
+            last->next = toAdd;
+            length++;
+        }
+
+        void displayList() {
+            Node *first = last->next;
+            while(first != last) {
+                cout << first->data << " ";
+                first = first->next;
+            }
+            cout << first->data << " ";
+            cout << endl;
+        }
+
+        int displayLength() {
+            return length;
+        }
 
 };
