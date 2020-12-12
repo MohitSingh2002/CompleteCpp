656389cea63ca8901e511862f6c2ecd586940b54
Mohit <mohitsingh11082002@gmail.com>
2020-12-12 16:09:17 +0530

    Addded more functionalities in the tree.

--- Data Structires And Algorithms/4_trees/1_trees.cpp
+++ Data Structires And Algorithms/4_trees/1_trees.cpp
@@ -2,6 +2,13 @@
 #include"1_trees_helper.h"
 using namespace std;
 
+void deleteTree(TreeNode<int> *root);
+// void postorder(TreeNode<int>*);
+void preorder(TreeNode<int>*);
+int leafNodesInTheTree(TreeNode<int>*);
+void printAtLevelK(TreeNode<int>*, int);
+// int heigthOfTree(TreeNode<int> *root);
+int lengthOfTree(TreeNode<int>*);
 TreeNode<int>* takeInputLevelWise();
 TreeNode<int>* takeInput();
 void printTree(TreeNode<int>*);
@@ -12,6 +19,17 @@ int main() {
     TreeNode<int> *root = takeInputLevelWise();
 
     printTree(root);
+
+    cout << "The Length of the entered tree is : " << lengthOfTree(root) << endl;
+
+    printAtLevelK(root, 2);
+
+    cout << "Total number of leaf nodes are : " << leafNodesInTheTree(root) << endl;
+
+    preorder(root);
+    cout << endl;
+
+    deleteTree(root);
     
     return 0;
 }
@@ -83,4 +101,57 @@ TreeNode<int>* takeInputLevelWise() {
         }
     }
     return root;
+}
+
+int lengthOfTree(TreeNode<int> *root) {
+    int answer = 1;
+    for(int i = 0; i < root->children.size(); i++) {
+        answer += lengthOfTree(root->children[i]);
+    }
+    return answer;
+}
+
+// int heigthOfTree(TreeNode<int> *root) {}
+
+void printAtLevelK(TreeNode<int> *root, int k) {
+    if(root == NULL) {
+        return;
+    }
+    if (k == 0) {
+        cout << root->data << endl;
+        return;
+    }
+    for(int i = 0; i < root->children.size(); i++) {
+        printAtLevelK(root->children[i], k-1);
+    }
+}
+
+int leafNodesInTheTree(TreeNode<int> *root) {
+    if(root->children.size() == 0) {
+        return 1;
+    }
+    int answer = 0;
+    for(int i = 0; i < root->children.size(); i++) {
+        answer += leafNodesInTheTree(root->children[i]);
+    }
+    return answer;
+}
+
+void preorder(TreeNode<int> *root) {
+    if(root == NULL) {
+        return;
+    }
+    cout << root->data << " ";
+    for(int i = 0; i < root->children.size(); i++) {
+        preorder(root->children[i]);
+    }
+}
+
+// void postorder(TreeNode<int> *root) {}
+
+void deleteTree(TreeNode<int> *root) {
+    for(int i = 0; i < root->children.size(); i++) {
+        deleteTree(root->children[i]);
+    }
+    delete root;
 }
