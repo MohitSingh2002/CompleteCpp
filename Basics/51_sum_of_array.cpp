99e921150c9154585774ea76478ca4604342a4f5
Mohit <mohitsingh11082002@gmail.com>
2020-12-14 16:59:23 +0530

    Added more code.

---
+++ Basics/51_sum_of_array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+      int n;
+      cin >> n;
+
+      int arr[n];
+
+      int sum = 0;
+
+      for(int i = 0; i < n; i++) {
+            cin >> arr[i];
+            sum += arr[i];
+      }
+
+      cout << "SUM : " << sum << endl;
+      
+      return 0;
+      
+}
