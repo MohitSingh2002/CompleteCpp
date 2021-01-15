0bde5ec4dbcf9d7b080e1b69cb2972121e3ee0f2
Mohit <mohitsingh11082002@gmail.com>
2021-01-15 18:07:30 +0530

    Added BFS.

--- Data Structires And Algorithms/7_graph/1_graph.cpp
+++ Data Structires And Algorithms/7_graph/1_graph.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void printDFS(int**, int, int, bool*);
+void printBFS(int**, int, int);
 
 int main() {
 
@@ -28,13 +29,23 @@ int main() {
             visitedVertex[i] = false;
       }
 
+      cout << "DFS :-" << " ";
       printDFS(edges, numberOfNodes, 0, visitedVertex);
+      cout << endl;
+
+      cout << "BFS :-" << " ";
+      printBFS(edges, numberOfNodes, 0);
+      cout << endl;
+
+      delete[] visitedVertex;
+      for(int i = 0; i < numberOfNodes; i++) delete [] edges[i];
+      delete [] edges;
 
       return 0;
 }
 
 void printDFS(int **edges, int n, int startingVertex, bool *visitedVertex) {
-      cout << startingVertex << endl;
+      cout << startingVertex << " ";
       visitedVertex[startingVertex] = true;
       for(int i=0;i<n;i++) {
             if(startingVertex == i) continue;
@@ -46,5 +57,31 @@ void printDFS(int **edges, int n, int startingVertex, bool *visitedVertex) {
 
       // This is DFS (Depth First Search).
       // Go in one direction untill it's end came.
+
+}
+
+void printBFS(int **edges, int n, int startingVertex) {
+      bool *visitedVertex = new bool[n];
+      for(int i=0;i<n;i++) {
+            visitedVertex[i] = false;
+      }
+      queue<int> pendingVertex;
+      pendingVertex.push(startingVertex);
+      visitedVertex[startingVertex] = true;
+      while(!pendingVertex.empty()) {
+            int currentVertex = pendingVertex.front();
+            pendingVertex.pop();
+            cout << currentVertex << " ";
+            for(int i = 0; i < n; i++) {
+                  if(currentVertex == i) continue;
+                  if(edges[currentVertex][i] == 1 && !visitedVertex[i]) {
+                        pendingVertex.push(i);
+                        visitedVertex[i] = true;
+                  }
+            }
+      }
+
+      // This is BFS (Breadth First Search).
+      // Same as level order traversal in Trees.
 
 }
